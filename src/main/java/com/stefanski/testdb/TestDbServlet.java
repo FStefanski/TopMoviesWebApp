abf51f7a141cbb330f05aaf51c7053e3803d2611
FrederikSte <frederik.stefanski@gmail.com>
2018-03-19 11:17:49 +0100

    Adding MySQL data base test

---
+++ src/main/java/com/stefanski/testdb/TestDbServlet.java
@@ -0,0 +1,54 @@
+package com.stefanski.testdb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestDbServlet
+ */
+@WebServlet("/TestDbServlet")
+public class TestDbServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// setup connection variables
+		String user = "topmoviesuser"; // springstudent
+		String password = "topmovies";
+
+		String jdbcUrl = "jdbc:mysql://localhost:3306/top_movies_collection?useSSL=false";
+		String driver = "com.mysql.jdbc.Driver";
+
+		// get connection to database
+		try {
+			PrintWriter out = response.getWriter();
+			out.println("Connection to databae: " + jdbcUrl);
+
+			Class.forName(driver);
+
+			Connection myConn = DriverManager.getConnection(jdbcUrl, user, password);
+
+			out.print("Success!");
+
+			myConn.close();
+
+		} catch (Exception exc) {
+			exc.printStackTrace();
+			throw new ServletException(exc);
+		}
+	}
+
+}
